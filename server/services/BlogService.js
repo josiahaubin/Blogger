0ef4ccac995b8fc8e5fde5d08452e860c0be29be
Josiah Aubin <josiahaubin@gmail.com>
2019-09-06 14:37:30 -0600

    Started set up

--- server/services/BlogService.js
+++ server/services/BlogService.js
@@ -4,12 +4,14 @@ const ObjectId = Schema.Types.ObjectId
 
 const _model = new Schema({
     title: { type: String, required: true },
-    description: { type: String, required: true },
+    summary: { type: String, required: true },
+    img: { type: String, required: true },
+    body: { type: String, required: true },
     authorId: { type: ObjectId, ref: 'User', required: true }
 }, { timestamps: true })
 
-export default class ValueService {
+export default class BlogService {
     get repository() {
-        return mongoose.model('value', _model)
+        return mongoose.model('blog', _model)
     }
 }
